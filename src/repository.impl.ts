db6a441a730b9dcee804da9bed5b7d402579064d
linhx <nguyendinhlinhx@gmail.com>
2023-01-10 23:04:43 +0700

    feat: implement saveAll, deleteAll, deleteById, deleteAllById, existsById, findAllById, findById

--- src/repository.impl.ts
+++ src/repository.impl.ts
@@ -1,9 +1,9 @@
-import { Repository } from '@linhx/nest-repo';
+import { Repository, Transaction } from '@linhx/nest-repo';
 import { PagingDto } from '@linhx/rest-common';
 import { Model } from 'mongoose';
 import { DbMongo, MongoTransaction } from './db.mongo';
 
-export class RepositoryImpl<T> implements Repository<T> {
+export class RepositoryImpl<T, U> implements Repository<T, U> {
   constructor(private _db: DbMongo, private _model: Model<any>) {}
   create(trx: MongoTransaction, t: T): Promise<T> {
     return this._db.withTransaction(trx, (_trx) => {
@@ -18,13 +18,48 @@ export class RepositoryImpl<T> implements Repository<T> {
     });
   }
   save(trx: MongoTransaction, t: T): Promise<T> {
-    throw new Error('Method not implemented.');
+    return this._db.withTransaction(trx, (_trx) => {
+      return this._model
+        .findByIdAndUpdate((t as any)._id, t, { session: _trx, upsert: true })
+        .exec();
+    });
+  }
+  saveAll(trx: MongoTransaction, ts: T[]): Promise<T[]> {
+    return this._db.withTransaction(trx, (_trx) => {
+      return Promise.all(ts.map((t) => this.save(_trx, t)));
+    });
   }
   delete(trx: MongoTransaction, t: T): Promise<T> {
     return this._db.withTransaction(trx, (_trx) => {
       return this._model
         .findByIdAndDelete((t as any)._id, { session: _trx })
         .exec();
+    });
+  }
+  deleteAll(trx: MongoTransaction): Promise<void> {
+    return this._db.withTransaction(trx, (_trx) => {
+      return this._model.deleteMany({}, { session: _trx }).exec();
+    });
+  }
+  deleteById(trx: MongoTransaction, id: U): Promise<void> {
+    return this._db.withTransaction(trx, (_trx) => {
+      return this._model.findByIdAndDelete(id, { session: _trx }).exec();
+    });
+  }
+  deleteAllById(trx: MongoTransaction, ids: U[]): Promise<void> {
+    return this._db.withTransaction(trx, (_trx) => {
+      return this._model
+        .deleteMany({ _id: { $in: ids } }, { session: _trx })
+        .exec();
+    });
+  }
+  existsById(trx: MongoTransaction, id: U): Promise<boolean> {
+    return this._db.withTransaction(trx, async (_trx) => {
+      const count = await this._model
+        .find({ _id: id }, undefined, { session: _trx })
+        .countDocuments()
+        .exec();
+      return count > 0;
     });
   }
   findAll(
@@ -39,6 +74,24 @@ export class RepositoryImpl<T> implements Repository<T> {
         query.limit(paging.limit).skip(paging.getSkip()).sort(paging.sort);
       }
       return query.exec();
+    });
+  }
+  findAllById(trx: MongoTransaction, ids: U[]): Promise<T[]> {
+    return this._db.withTransaction(trx, async (_trx) => {
+      return this._model
+        .find({ _id: { $in: ids } }, undefined, {
+          session: _trx,
+        })
+        .exec();
+    });
+  }
+  findById(trx: MongoTransaction, id: U): Promise<T> {
+    return this._db.withTransaction(trx, async (_trx) => {
+      return this._model
+        .findById(id, undefined, {
+          session: _trx,
+        })
+        .exec();
     });
   }
   count(trx: MongoTransaction, condition?: Partial<T>): Promise<number> {
