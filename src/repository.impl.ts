ea5da91eab9084da0c8e78fe053015bf064efd77
linhx <nguyendinhlinhx@gmail.com>
2023-05-09 23:20:20 +0700

    feat: remove transaction from parameter, use AsyncLocalStorage instead

--- src/repository.impl.ts
+++ src/repository.impl.ts
@@ -1,108 +1,133 @@
-import { Repository, Transaction } from '@linhx/nest-repo';
+import { Repository, TransactionStore } from '@linhx/nest-repo';
 import { PagingDto } from '@linhx/rest-common';
-import { Model } from 'mongoose';
-import { DbMongo, MongoTransaction } from './db.mongo';
+import { ClientSession, Model } from 'mongoose';
+import { DbMongo } from './db.mongo';
 
 export class RepositoryImpl<T, U> implements Repository<T, U> {
-  constructor(private _db: DbMongo, private _model: Model<any>) {}
-  create(trx: MongoTransaction, t: T): Promise<T> {
-    return this._db.withTransaction(trx, async (_trx) => {
-      const [entity] = await this._model.create([t], { session: _trx });
-      return entity;
+  constructor(
+    private _db: DbMongo,
+    private _model: Model<any>,
+    private transactionStore?: TransactionStore
+  ) {}
+  async create(t: T): Promise<T> {
+    const [entity] = await this._model.create([t], {
+      session: this.transactionStore.getTransaction(),
+    });
+    return entity;
+  }
+  createMany(...ts: T[]): Promise<T[]> {
+    return this._model.create(ts, {
+      session: this.transactionStore.getTransaction(),
     });
   }
-  createMany(trx: MongoTransaction, ...ts: T[]): Promise<T> {
-    return this._db.withTransaction(trx, (_trx) => {
-      return this._model.create(ts, { session: _trx });
+  update(t: T): Promise<T> {
+    return new Promise((resolve, reject) => {
+      this._model.findByIdAndUpdate(
+        (t as any)._id,
+        t,
+        {
+          session: this.transactionStore.getTransaction() as ClientSession,
+        },
+        (err, doc) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(doc);
+          }
+        }
+      );
     });
   }
-  update(trx: MongoTransaction, t: T): Promise<T> {
-    return this._db.withTransaction(trx, (_trx) => {
-      return this._model
-        .findByIdAndUpdate((t as any)._id, t, { session: _trx })
-        .exec();
+  save(t: T): Promise<T> {
+    return new Promise((resolve, reject) => {
+      this._model.findByIdAndUpdate(
+        (t as any)._id,
+        t,
+        {
+          session: this.transactionStore.getTransaction() as ClientSession,
+          upsert: true,
+        },
+        (err, doc) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(doc);
+          }
+        }
+      );
     });
   }
-  save(trx: MongoTransaction, t: T): Promise<T> {
-    return this._db.withTransaction(trx, (_trx) => {
-      return this._model
-        .findByIdAndUpdate((t as any)._id, t, { session: _trx, upsert: true })
-        .exec();
-    });
+  saveAll(ts: T[]): Promise<T[]> {
+    return Promise.all(ts.map((t) => this.save(t)));
   }
-  saveAll(trx: MongoTransaction, ts: T[]): Promise<T[]> {
-    return this._db.withTransaction(trx, (_trx) => {
-      return Promise.all(ts.map((t) => this.save(_trx, t)));
-    });
+  delete(t: T): Promise<T> {
+    return this._model
+      .findByIdAndDelete((t as any)._id, {
+        session: this.transactionStore.getTransaction() as ClientSession,
+      })
+      .exec();
   }
-  delete(trx: MongoTransaction, t: T): Promise<T> {
-    return this._db.withTransaction(trx, (_trx) => {
-      return this._model
-        .findByIdAndDelete((t as any)._id, { session: _trx })
-        .exec();
-    });
+  async deleteAll(): Promise<void> {
+    await this._model
+      .deleteMany(
+        {},
+        { session: this.transactionStore.getTransaction() as ClientSession }
+      )
+      .exec();
   }
-  deleteAll(trx: MongoTransaction): Promise<void> {
-    return this._db.withTransaction(trx, (_trx) => {
-      return this._model.deleteMany({}, { session: _trx }).exec();
-    });
+  async deleteById(id: U): Promise<void> {
+    await this._model
+      .findByIdAndDelete(id, {
+        session: this.transactionStore.getTransaction() as ClientSession,
+      })
+      .exec();
   }
-  deleteById(trx: MongoTransaction, id: U): Promise<void> {
-    return this._db.withTransaction(trx, (_trx) => {
-      return this._model.findByIdAndDelete(id, { session: _trx }).exec();
-    });
+  async deleteAllById(ids: U[]): Promise<void> {
+    await this._model
+      .deleteMany(
+        { _id: { $in: ids } },
+        { session: this.transactionStore.getTransaction() as ClientSession }
+      )
+      .exec();
   }
-  deleteAllById(trx: MongoTransaction, ids: U[]): Promise<void> {
-    return this._db.withTransaction(trx, (_trx) => {
-      return this._model
-        .deleteMany({ _id: { $in: ids } }, { session: _trx })
-        .exec();
-    });
+  async existsById(id: U): Promise<boolean> {
+    const count = await this._model
+      .find({ _id: id }, undefined, {
+        session: this.transactionStore.getTransaction() as ClientSession,
+      })
+      .countDocuments()
+      .exec();
+    return count > 0;
   }
-  existsById(trx: MongoTransaction, id: U): Promise<boolean> {
-    return this._db.withTransaction(trx, async (_trx) => {
-      const count = await this._model
-        .find({ _id: id }, undefined, { session: _trx })
-        .countDocuments()
-        .exec();
-      return count > 0;
-    });
-  }
-  findAll(
-    trx: MongoTransaction,
-    condition?: Partial<T>,
-    paging?: PagingDto
-  ): Promise<T[]> {
-    return this._db.withTransaction(trx, (_trx) => {
-      const query = this._model.find(condition).session(_trx);
+  findAll(condition?: Partial<T>, paging?: PagingDto): Promise<T[]> {
+    const query = this._model
+      .find(condition)
+      .session(this.transactionStore.getTransaction() as ClientSession);
 
-      if (paging) {
-        query.limit(paging.limit).skip(paging.getSkip()).sort(paging.sort);
-      }
-      return query.exec();
-    });
+    if (paging) {
+      query.limit(paging.limit).skip(paging.getSkip()).sort(paging.sort);
+    }
+    return query.exec();
   }
-  findAllById(trx: MongoTransaction, ids: U[]): Promise<T[]> {
-    return this._db.withTransaction(trx, async (_trx) => {
-      return this._model
-        .find({ _id: { $in: ids } }, undefined, {
-          session: _trx,
-        })
-        .exec();
-    });
+  findAllById(ids: U[]): Promise<T[]> {
+    return this._model
+      .find({ _id: { $in: ids } }, undefined, {
+        session: this.transactionStore.getTransaction() as ClientSession,
+      })
+      .exec();
   }
-  findById(trx: MongoTransaction, id: U): Promise<T> {
-    return this._db.withTransaction(trx, async (_trx) => {
-      return this._model
-        .findById(id, undefined, {
-          session: _trx,
-        })
-        .exec();
-    });
+  findById(id: U): Promise<T> {
+    return this._model
+      .findById(id, undefined, {
+        session: this.transactionStore.getTransaction() as ClientSession,
+      })
+      .exec();
   }
-  count(trx: MongoTransaction, condition?: Partial<T>): Promise<number> {
-    return this._db.withTransaction(trx, (_trx) => {
-      return this._model.countDocuments(condition, { session: _trx }).exec();
-    });
+  count(condition?: Partial<T>): Promise<number> {
+    return this._model
+      .countDocuments(condition, {
+        session: this.transactionStore.getTransaction() as ClientSession,
+      })
+      .exec();
   }
 }
